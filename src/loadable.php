f721acf0d698da5eb3a91b7000c0aceb8016e569
Emiel Vandenbussche <emiel.vandenbussche@zapfi.com>
2015-07-07 13:42:53 +0200

    initial commit

--- src/loadable.php
+++ src/loadable.php
@@ -5,3 +5,18 @@
  * Date: 7/07/2015
  * Time: 13:24
  */
+namespace emiel;
+
+class loadable
+{
+
+
+    function __construct()
+    {
+
+    }
+
+    function echoIt(){
+        echo "hoera het werkt kerels";
+    }
+}
